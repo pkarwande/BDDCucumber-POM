376ed2d99646c0f576a4ebcc43604e45f5ca7129
Prachiti karwande <Prachiti karwande@DESKTOP-9F7UL5A>
2020-07-27 15:25:06 +0530

    Committing remaining changes

--- CucumberBDD/src/test/java/Runner/TestRunner.java
+++ CucumberBDD/src/test/java/Runner/TestRunner.java
@@ -7,20 +7,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.junit.Cucumber;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features/laptop.feature",
+//@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Prachiti karwande\\git\\BDDCucumber-POM\\CucumberBDD\\src\\test\\resources\\Features\\laptop.feature",
 glue= {"com/ecommerce/CucumberBDD"},
 monochrome=true,
-plugin = {"pretty", "html:test-output"},
+plugin = {"pretty", "html:target/test-output"},
 dryRun = false )
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 	
-   /*  private TestNGCucumberRunner testNGCucumberRunner;  
+  /* private TestNGCucumberRunner testNGCucumberRunner;  
 
 	  @BeforeClass(alwaysRun = true)  
 	    public void setUpClass() throws Exception {  
